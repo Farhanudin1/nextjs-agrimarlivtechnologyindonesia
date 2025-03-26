8cdc784e189a3ff8356b567b025067c9856c23e4
Farhan Khoirudin <127929607+Farhanudin1@users.noreply.github.com>
2025-03-26 16:45:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -562,7 +562,7 @@ export default function Home() {
                     <div className="mr-4 rounded-full bg-green-100 p-2">
                       <svg
                         className="h-4 w-4 text-green-600"
-                        xmlns="bg3.png"
+                        xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
