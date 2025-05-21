775edd0d371142a564acabbc959467fa654e4364
Farhan Khoirudin <127929607+Farhanudin1@users.noreply.github.com>
2025-05-21 11:13:24 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1266,7 +1266,7 @@ export default function Home() {
                   </svg>
                   <span className="sr-only">Facebook</span>
                 </Link>
-                <Link href="#" className="text-muted-foreground hover:text-primary">
+                <Link href="https://www.instagram.com/agrimarlivtechnology/" className="text-muted-foreground hover:text-primary">
                   <svg
                     className="h-5 w-5"
                     xmlns="http://www.w3.org/2000/svg"
@@ -1296,7 +1296,6 @@ export default function Home() {
                   >
                     <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
                   </svg>
-                  <a href="https://www.instagram.com/farhan_khoirudin" class="text-white mx-3"><i class="bi bi-instagram"></i></a>
                   <span className="sr-only">Twitter</span>
                 </Link>
                 <Link href="#" className="text-muted-foreground hover:text-primary">
